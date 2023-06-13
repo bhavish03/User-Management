be76f7fd8cd719e4d852704a4ade88a63d4bedda
Bhavish <bhavish.muneshwar@rasonix.com>
2023-06-13 13:21:09 +0530

    project started with backend and frontend setup done

---
+++ Frontend/User_Management/src/App.tsx
@@ -0,0 +1,13 @@
+
+
+
+
+function App() {
+
+
+  return <div className="flex justify-center bg-yellow-500">
+   <h1 className="text-3xl font-bold underline">Hello</h1> 
+  </div>;
+}
+
+export default App;
