63fd944f1fa22fee0fa30c81a19e60f884e52adc
Bhavish <bhavish.muneshwar@rasonix.com>
2023-06-13 13:31:20 +0530

    first commit

--- Frontend/User_Management/src/App.tsx
+++ Frontend/User_Management/src/App.tsx
@@ -6,7 +6,7 @@ function App() {
 
 
   return <div className="flex justify-center bg-yellow-500">
-   <h1 className="text-3xl font-bold underline">Hello</h1> 
+   <h1 className="text-3xl font-bold underline">Hello </h1> 
   </div>;
 }
 
